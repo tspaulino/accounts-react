6ce88e7b2c84074db5f6af42c9ff4e1d71c03da3
Hugo W <hugwnt@gmail.com>
2018-01-14 13:38:30 -0200

    Refactor Auth HOC

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
+import * as actions from '../../redux/auth'
 import './Auth.scss'
 
-const Auth = ComposedComponent => class extends Component {
-  componentWillMount() {}
+const AuthWrapper = (ComposedComponent) => {
+  class Auth extends Component {
+    componentWillMount() {}
 
-  render() {
-    return (
-      <div className="auth">
-        <ComposedComponent {...this.props} />
-      </div>
-    )
+    render() {
+      return (
+        <div className="auth">
+          <ComposedComponent {...this.props} />
+        </div>
+      )
+    }
   }
+
+  const mapStateToProps = ({ auth }) => ({ auth })
+  const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(actions, dispatch) })
+
+  return connect(mapStateToProps, mapDispatchToProps)(Auth)
 }
 
-export default Auth
+export default AuthWrapper
