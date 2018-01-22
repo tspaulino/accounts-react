4f61914c7fb6b2305e6320cea12b7d976270bc5b
Hugo W <hugwnt@gmail.com>
2018-01-22 20:20:06 -0200

    Add request error handler

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -18,3 +18,39 @@ export const getValidationErrors = (errors) => {
  * getResponseError
  */
 export const getResponseError = error => ({ content: error.message, code: error.statusCode })
+
+/**
+ * toValidationError
+ */
+const toValidationError = (errors) => {
+  const keys = Object.keys(errors)
+  return keys.reduce((obj, item) => ({ ...obj, ...{ [item]: [errors[item].message] } }), {})
+}
+
+/**
+ * requestErrorHandler
+ */
+export const requestErrorHandler = (reqError) => {
+  const error = {
+    type: 'requestError' // requestError, responseError, validationError
+  }
+  const { response, request } = reqError
+
+  if (response) {
+    const { data } = response
+
+    if (data.errors) {
+      error.validation = toValidationError(data.errors)
+      error.type = 'validationError'
+    } else {
+      error.message = data.message
+      error.type = 'responseError'
+    }
+  } else if (request) {
+    error.message = 'Request error'
+  } else {
+    error.message = reqError.message
+  }
+
+  return error
+}
